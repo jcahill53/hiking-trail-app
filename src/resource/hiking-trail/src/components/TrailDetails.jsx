9df9977536b50e13cc30d0bd781802f1d1b344ac
Joann Cahill <cahilljm@aol.com>
2022-09-06 05:06:18 -0700

    Push addition of Update Endpoint on Trail Details

--- src/resource/hiking-trail/src/components/TrailDetails.jsx
+++ src/resource/hiking-trail/src/components/TrailDetails.jsx
@@ -3,6 +3,14 @@ import { useParams } from 'react-router-dom';
 import { Link } from 'react-router-dom';
 import axios from "axios";
 import TrailComments from "./TrailComments"
+import Background from '../images/austin-ban-juHayWuaaoQ-unsplash.jpg'
+
+/* ARTHUR summary of changes:
+** - Added const trailHeaderSectionStyle with proper variables
+** - Added class trail-header-section and background image
+** - Add className="trail-details"
+** - Add Trail Details section to app.css
+*/
 
 
 function TrailDetails() {
@@ -14,6 +22,14 @@ function TrailDetails() {
     const [trailParking, setTrailParking] = useState([]);
     const [isLoading, setIsLoading] = useState(true);
     const [hasError, setHasError] = useState(false);
+    const [status, setStatus] = useState('');
+
+    const trailHeaderSectionStyle = {
+        backgroundImage: `url(${Background})`,
+        backgroundPosition: 'top',
+        backgroundRepeat: 'no-repeat',
+        backgroundSize: 'contain'
+    }
 
     // url variables
 
@@ -30,6 +46,7 @@ function TrailDetails() {
     // parking for a trail api
     const parkingUrl = `http://localhost:5000/hikingtrails/${trailId}/parking`;
     console.log(parkingUrl);
+
 
     //    fetch data for both hero and comics for the hero
     useEffect(() => {
@@ -68,7 +85,6 @@ function TrailDetails() {
         return (
             <div className="wait">
                 <p className="load">Loading...</p>
-
             </div>
         )
     }
@@ -85,74 +101,152 @@ function TrailDetails() {
     const usersHere = trailParking[0].usersThere
     const fullestDTTM = trailParking[0].fullest_day_time
     const emptiestDTTM = trailParking[0].emptiestDayTime
+    const parkingId = trailParking[0]._id
 
+    // Handle posting status on submit
+    const handlePost = async (event) => {
+        event.preventDefault();
 
+        const data = {
+            "parkingLotStatus": status,
+        }
 
- 
+        // process the registration
+        fetch(`http://localhost:5000/hikingtrails/${trailId}/parking/${parkingId}`, {
+            method: 'PUT', // or 'PUT'
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(data),
+        })
+            .then((response) => response.json())
+            .then((data) => {
+                console.log('Success:', data);
+            })
+            .catch((error) => {
+                console.error('Error:', error);
+            });
+    }
 
 
 
     return (
-        <section>
-            <section className="trail-info">
-                <h2 className="app-name">ParkNHike</h2>
-                <h1 className="detail-hdr">{trailDetails.name}</h1>
+        <div className="trail-details">
+            <section className="trail-header-section" style={trailHeaderSectionStyle}>
+                <section className="trail-info">
 
-                <p>${trailDetails.descr}</p>
+                    <h2 className="app-name">ParkNHike</h2>
+                    <div className="trail-info-container">
+                        <h1 className="detail-hdr">{trailDetails.name}</h1>
+
+                        <div className="trail-stats">
+                            <span><img id="location-pin" src="/pin.png" alt="" />
+                                {trailDetails.locations.latitude}, {trailDetails.locations.longitude}
+                            </span>
+
+                            <span><img id="trail-distance" src="/travel.png" alt="" />
+                                {trailDetails.measures.distance.value} {trailDetails.measures.distance.measure}
+                            </span>
+
+                            <span><img id="trail-difficulty" src="/speedometer.png" alt="" />
+                                {trailDetails.measures.difficulty}
+                            </span>
+                        </div>
+
+
+                        <p>{trailDetails.descr}</p>
+                    </div>
+                </section>
             </section>
 
-            <section className="parking-info">
-                <h2 className="detail-hero-comics">ParkNHike Real Time Statistics</h2>
-                <h2 className="app-name"> </h2>
-                <p>{parkingName}</p>
-                <div>
-                    <div>
-                        <p>Parking Lot Status</p>
-                        <p>{parkingStatus}</p>
+            <section className="trail-details parking-info trail-info-container">
+
+                <h2 className="trail-section-hdr"><span>ParkNHike</span> Real Time Statistics</h2>
+
+                <h2 className="section-sub-hdr">{parkingName}</h2>
+                <div className="column" >
+                    <div className="row parking-data">
+                        <div >
+                            <p className="park-label">Parking Lot Status</p>
+                            <p className="park-value">{parkingStatus}</p>
+                        </div>
+
+                        <div>
+                            <p className="park-label">Parking Type</p>
+                            <p className="park-value">{parkingType}</p>
+                        </div>
+
+                        <div>
+                            <p className="park-label">App Users Here</p>
+                            <p className="park-value">{usersHere}</p>
+                        </div>
                     </div>
 
-                    <div>
-                        <p>Parking Type</p>
-                        <p>{parkingType}</p>
-                    </div>
+                    <div className="row parking-data">
+                        <div>
+                            <p className="park-label">Emptiest Day/Time</p>
+                            <p className="park-value">{fullestDTTM}</p>
+                        </div>
 
-                    <div>
-                        <p>App Users Here</p>
-                        <p>{usersHere}</p>
+                        <div>
+                            <p className="park-label">Emptiest Day/Time</p>
+                            <p className="park-value">{emptiestDTTM}</p>
+                        </div>
+
+
                     </div>
                 </div>
 
-                <div>
-                    <div>
-                        <p>Emptiest Day/Time</p>
-                        <p>{fullestDTTM}</p>
-                    </div>
-
-                    <div>
-                        <p>Emptiest Day/Time</p>
-                        <p>{emptiestDTTM}</p>
-                    </div>
-
-
-                </div>
-
-
             </section>
-            <h1> User Posted Comments</h1>
-            <Link to={`/createmessage/${trailId}`}><button  >Post your own comment</button></Link>
-            <section className="comment-info">
-                <article className="row comment-card" >
-                    {trailComments.map((comment, id) =>
-                        <TrailComments
-                            key={id}
-                            comment={comment}
-                            messageBody={comment.messageBody}
 
-                        />
-                    )}
-                </article>
+            <section className="trail-details parking-info trail-info-container">
+                <section className="trail-details report-park-status trail-info-container">
+                    <div >
+                        <div>
+                            <h2 className="trail-section-hdr">Report Changes in Parking Status</h2>
+                        </div>
+
+
+                        <form className="report-status-form">
+                            {/* Labels and inputs for form data */}
+
+                            <div className="form-container">
+                                <label className="status-label">Select the Current Parking Status
+                                    <input className="status-select"
+                                        onChange={(e) => setStatus(e.target.value)}
+                                        value={status}
+                                        type="text"
+                                        autoComplete="on"
+                                        required
+                                    /></label>
+                            </div>
+                            <div className="row button-container">
+                                <button onClick={handlePost} className="welcome-button" type="submit">
+                                    Post the Status of Parking
+                                </button>
+                            </div>
+                        </form>
+                    </div>
+                </section>
             </section>
-        </section>
+
+            <section className="trail-details user-comments trail-info-container">
+                <h2 className="trail-section-hdr"><span>ParkNHike</span>User Posted Comments</h2>
+                <Link to={`/createmessage/${trailId}`}><button  >Post your own comment</button></Link>
+                <section className="comment-info">
+                    <article className="column comment-card" >
+                        {trailComments.map((comment, id) =>
+                            <TrailComments
+                                key={id}
+                                comment={comment}
+                                messageBody={comment.messageBody}
+
+                            />
+                        )}
+                    </article>
+                </section>
+            </section>
+        </div>
     )
 }
 
